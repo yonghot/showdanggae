0ced3497f8ed2478d705b3bdb4ba1d1ec208a3b9
kosta <kosta@kosta-PC>
2015-12-23 13:20:07 +0900

    sh

--- showdanggae/src/main/java/org/kosta/finalproject/controller/ProductController.java
+++ showdanggae/src/main/java/org/kosta/finalproject/controller/ProductController.java
@@ -18,6 +18,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * @author kosta
+ *
+ */
 @Controller
 public class ProductController {
 
